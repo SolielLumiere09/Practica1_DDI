80f82ee89cadc874011e53fca40b3ccb5c8053b7
SolielLuimiere <54183914+SolielLumiere09@users.noreply.github.com>
2020-09-26 15:04:09 -0700

    implementacion
    Se implemento la funcion

---
+++ Assets/Scenes/CubeScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i;
+        int[] list;
+        list = listOfMinsEach(new int[5]{8,1,2,2,3});
+
+        for(i = 0; i < list.Length; i++)
+        {
+            Debug.Log(list[i] + " ");
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    int[] listOfMinsEach(int [] list)
+    {
+        int[] auxList = new int[list.Length];
+        int count = 0;
+        int i, j;
+
+        for(i = 0; i < list.Length; i++)
+        {
+            
+            for(j = 0; j < list.Length; j++)
+            {
+                if(list[j] < list[i])
+                    count++;
+
+            }
+
+            auxList[i] = count;
+            count = 0;
+
+        }
+        
+
+        return auxList;
+    }
+}
